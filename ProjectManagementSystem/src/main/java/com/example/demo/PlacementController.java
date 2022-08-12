5bc26899adf1763085dd0c96b7890bac2ea24614
NAMRATA NAVGHARE <NAMRATA NAVGHARE@DESKTOP-5NJ6TES>
2022-08-12 17:36:08 +0530

    project

---
+++ ProjectManagementSystem/src/main/java/com/example/demo/PlacementController.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+//import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+//@Repository
+@RestController
+public class PlacementController {
+
+	@Autowired
+	private PlacementService service;
+	@GetMapping("/placement")
+	 public List<Placement> list() 
+	 {
+	 return service.listAll();
+	 }
+	 @GetMapping("/placement/{id}")
+	 public ResponseEntity<Placement> get(@PathVariable int id) 
+	 {
+	 try
+	 {
+		 Placement placement = service.get(id);
+	 return new ResponseEntity<Placement>(placement, HttpStatus.OK);
+	 } 
+	 catch (NoSuchElementException e) 
+	 {
+	 return new ResponseEntity<Placement>(HttpStatus.NOT_FOUND);
+	 } 
+	 }
+	 // RESTful API method for Create operation
+	 @PostMapping("/placement")
+	 public void add(@RequestBody Placement placement) 
+	 {
+	
+	service.save(placement);
+	 }
+	 
+	 // RESTful API method for Update operation
+	 @PutMapping("/placement/{id}")
+	 public ResponseEntity<?> update(@RequestBody Placement placement, @PathVariable Long id) 
+	 {
+	 try
+	 {
+		 
+	 service.save(placement);
+	 return new ResponseEntity<>(HttpStatus.OK);
+	 } 
+	 catch (NoSuchElementException e) 
+	 {
+	 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	 } 
+	 }
+	 // RESTful API method for Delete operation
+	 @DeleteMapping("/placement/{id}")
+	 public void delete(@PathVariable int id) 
+	 {
+	 service.delete(id);
+	 }
+	
+	
+}
